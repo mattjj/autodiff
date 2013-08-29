58e4a1da1877f22a28949e908f0c24a98a99b3a3
Matthew Johnson <mattjj@csail.mit.edu>
2013-08-29 15:23:36 -0400

    changed line order

--- forward_mode.jl
+++ forward_mode.jl
@@ -8,6 +8,8 @@ type DualNumber <: Number
 end
 
 eps(x::Union(DualNumber,AbstractArray{DualNumber})) = map(x -> x.eps, x)
+convert(::Type{DualNumber},x::Real) = DualNumber(x)
+promote_rule{T <: Real}(::Type{DualNumber},::Type{T}) = DualNumber
 
 +(x::DualNumber,y::DualNumber) = DualNumber(x.real + y.real,x.eps + y.eps)
 -(x::DualNumber,y::DualNumber) = DualNumber(x.real - y.real,x.eps - y.eps)
@@ -17,9 +19,6 @@ eps(x::Union(DualNumber,AbstractArray{DualNumber})) = map(x -> x.eps, x)
 sin(x::DualNumber) = DualNumber(sin(x.real),cos(x.real)*x.eps)
 cos(x::DualNumber) = DualNumber(cos(x.real),-sin(x.real)*x.eps)
 exp(x::DualNumber) = DualNumber(exp(x.real),exp(x.real)*x.eps)
-
-convert(::Type{DualNumber},x::Real) = DualNumber(x)
-promote_rule{T <: Real}(::Type{DualNumber},::Type{T}) = DualNumber
 
 function D(f::Function)
     function Df{T <: Real}(pt::T)
